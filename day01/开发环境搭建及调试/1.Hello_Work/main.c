33af1f33cecb5979d51e2c2f4a91e4260f590c0e
peiyandi <482534124@qq.com>
2022-11-20 18:05:31 +0800

    second commit

--- day01/开发环境搭建及调试/1.Hello_Work/main.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main() {
-	printf("Hello_World\n");
-	system("pause");
-
-	return 0;
-}
--- day01/数据类型与运算符/day02_常量与变量/main.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-#define PI 3	//PIÊÇ·ûºÅ³£Á¿
-int main() {
-	printf("%d\n", PI);
-}
